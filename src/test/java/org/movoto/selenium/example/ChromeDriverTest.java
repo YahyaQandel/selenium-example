570e80aa19df1c019441a75af6b3145b78bd2821
YahyaQandel <yahya.qandel@gmail.com>
2019-09-11 02:55:26 +0200

    # refactor chrome tests # add firefox and safari to gitignore

--- src/test/java/org/movoto/selenium/example/ChromeDriverTest.java
+++ src/test/java/org/movoto/selenium/example/ChromeDriverTest.java
@@ -2,22 +2,15 @@ package org.movoto.selenium.example;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.MarionetteDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -27,6 +20,8 @@ import static org.junit.Assert.assertTrue;
  */
 public class ChromeDriverTest {
 
+    public static final String ACCOUNT_USERNAME = "Yahya";
+    public static final String ACCOUNT_PHONENUMBER = "01090614633";
     private String testUrl;
     private WebDriver driver;
 
@@ -54,36 +49,38 @@ public class ChromeDriverTest {
     }
 
     @Test
-    public void testTitle() throws IOException {
-
-        // Find elements by attribute lang="READ_MORE_BTN"
-        List<WebElement> loginLayoutBtn = driver
-                .findElements(By.id("innerLoginBtn"));
-        //Click the selected button
-        loginLayoutBtn.get(0).click();
-
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-        WebElement loginNumberField = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.id("loginNum")));
-
-        loginNumberField.sendKeys("01090614633");
-
-
-        WebElement loginPasswordField = driver
-                .findElements(By.id("loginPassword")).get(0);
-        loginPasswordField.sendKeys("oNe_status_1");
-//
-        WebElement loginBtn = driver
-                .findElements(By.id("loginButton")).get(0);
-        loginBtn.click();
-
-
-        WebElement accountHeadingTitle = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.className("service-selector__active-heading")));
+    public void testAccountLogin() throws IOException {
+        openLoginLayout();
+        loginToAccount();
+        WebElement accountHeadingTitle = waitForElementToBeVisible(By.className("service-selector__active-heading"));
         String AccountHeadingTitle = accountHeadingTitle.getText();
-        assertTrue(AccountHeadingTitle.contains("Yahya"));
-        assertTrue(AccountHeadingTitle.contains("01090614633"));
+        assertAccountLoginDetailsAreExists(AccountHeadingTitle);
 
+    }
+
+    private void loginToAccount() {
+        WebElement loginNumberField = waitForElementToBeVisible(By.id("loginNum"));
+        loginNumberField.sendKeys("01090614633");
+        WebElement loginPasswordField = waitForElementToBeVisible(By.id("loginPassword"));
+        loginPasswordField.sendKeys("oNe_status_1");
+        WebElement loginBtn = waitForElementToBeVisible(By.id("loginButton"));
+        loginBtn.click();
+    }
+
+    private void openLoginLayout() {
+        WebElement loginLayoutBtn = waitForElementToBeVisible(By.id("innerLoginBtn"));
+        loginLayoutBtn.click();
+    }
+
+    private void assertAccountLoginDetailsAreExists(String accountHeadingTitle) {
+        assertTrue(accountHeadingTitle.contains(ACCOUNT_USERNAME));
+        assertTrue(accountHeadingTitle.contains(ACCOUNT_PHONENUMBER));
+    }
+
+    private WebElement waitForElementToBeVisible(By selector) {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        return wait.until(
+                ExpectedConditions.visibilityOfElementLocated(selector));
     }
 
     @After
